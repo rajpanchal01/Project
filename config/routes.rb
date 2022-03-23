21c47c0219ac32fb47e44e5d93e2eaf548ae3821
PriyankaManna <priyanka.manna@bacancy.com>
2022-03-23 11:02:53 +0530

    database.yml file update

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#homepage"
   get "/home" ,to: "home#homepage"
-  authenticate :user, ->(u) { u.has_role? :admin } do
+  authenticate :user, ->(u) { u.has_role? :employee } do
    # get 'guide/dashboard', :to => 'guides#dashboard'
    get "/dashboard" ,to: "home#dashboard"
   end
-  authenticate :user, ->(u) { u.has_role? :employee } do
-    get "/dashboard" ,to: "home#homepage"
-  end
+  # authenticate :user, ->(u) { u.has_role? :employee } do
+  #   get "/dashboard" ,to: "home#homepage"
+  # end
   #get "/dashboard" ,to: "home#dashboard"
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'  
