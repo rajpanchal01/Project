e0de9cb5fa51f5641efd214be28e14782786e091
Smit Akhani <smit.akhani@bacancy.com>
2022-04-05 18:25:45 +0530

    add team member implemented

--- app/controllers/project_masters_controller.rb
+++ app/controllers/project_masters_controller.rb
@@ -71,6 +71,7 @@ class ProjectMastersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_project_master
       @project_master = ProjectMaster.find(params[:id])
+      @project = ProjectMaster.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
