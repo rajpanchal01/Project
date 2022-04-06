5fe4d56c5940febeb708ba2eb74d8d59c005a710
rajpanchal01 <rajpanchal2810@gmail.com>
2022-04-06 11:14:14 +0530

    Mailer added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,15 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.cache_classes = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'panchaldilipkumar55@gmail.com',
+      :password             => '8200152658',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -40,7 +49,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
